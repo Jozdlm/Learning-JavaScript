08ff7ed12d0e001f1b25c50bfe572b36709e6c4e
Jozuan Lopez <jozuanlopez@gmail.com>
2023-08-14 21:06:58 -0600

    Añade la propiedad id a los elementos de la lista courses

--- shopping-cart/js/app.js
+++ shopping-cart/js/app.js
@@ -3,41 +3,49 @@ const cart = document.querySelector("#carrito");
 
 const courses = [
   {
+    id: 1,
     thumb: "img/curso1.jpg",
     title: "HTML5, CSS3, JavaScript para Principiantes",
     instructor: "Juan Pedro",
   },
   {
+    id: 2,
     thumb: "img/curso2.jpg",
     title: "Curso de Comida Vegetariana",
     instructor: "Maria Alvarado",
   },
   {
+    id: 3,
     thumb: "img/curso3.jpg",
     title: "Guitarra para Principiantes",
     instructor: "Otto Pérez",
   },
   {
+    id: 4,
     thumb: "img/curso4.jpg",
     title: "Huerto en tu casa",
     instructor: "Estrella de León",
   },
   {
+    id: 5,
     thumb: "img/curso5.jpg",
     title: "Decoración con productos de tu hogar",
     instructor: "Maria Ortega",
   },
   {
+    id: 6,
     thumb: "img/curso1.jpg",
     title: "Diseño Web para Principiantes",
     instructor: "Gustavo Hernandez",
   },
   {
+    id: 7,
     thumb: "img/curso2.jpg",
     title: "Comida Mexicana para principiantes",
     instructor: "Maria Alvarado",
   },
   {
+    id: 8,
     thumb: "img/curso3.jpg",
     title: "Estudio Musical en tu casa",
     instructor: "Juan Pedro",
@@ -56,7 +64,7 @@ function main() {
         <a
           href="#"
           class="u-full-width button-primary button input agregar-carrito"
-          data-id="1"
+          data-id="${item.id}"
           >Agregar Al Carrito</a
         >
       </div>
