30c77a5b757121ad174eabe408927dfc82d9e9af
Jozuan Lopez <jozuanlopez@gmail.com>
2023-08-14 20:28:39 -0600

    Define array courses para almacenar el listado de los cursos

---
+++ shopping-cart/js/app.js
@@ -0,0 +1,10 @@
+const coursesWrapper = document.querySelector("#courses-wrapper");
+const cart = document.querySelector("#carrito");
+
+const courses = [
+  {
+    thumb: "img/curso1.jpg",
+    title: "HTML5, CSS3, JavaScript para Principiantes",
+    instructor: "Juan Pedro",
+  },
+];
