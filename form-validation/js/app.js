f90cfaca1b74773ef929a90b1185abc0efef709e
Jozuan Lopez <jozuanlopez@gmail.com>
2023-08-16 21:17:26 -0600

    Muestra en el HTML la alerta de error cuando el input no pasa la validación

--- form-validation/js/app.js
+++ form-validation/js/app.js
@@ -1,14 +1,25 @@
 const emailInput = document.getElementById("email");
 const asuntoInput = document.getElementById("asunto");
 const mensajeInput = document.getElementById("mensaje");
+const contactForm = document.getElementById("formulario");
 
 function requiredValidator(e) {
-  if (e.target.value.trim() === "") {
-    console.log("El campo x es requerido");
+  const { name, value } = e.target;
+
+  if (value.trim() === "") {
+    showAlert(`El campo ${name} es requerido`);
   }
 }
 
 function emailValidator() {}
+
+function showAlert(message) {
+  const error = document.createElement("p");
+  error.textContent = message;
+  error.classList.add("bg-red-600", "text-white", "p-2", "text-center");
+
+  contactForm.appendChild(error);
+}
 
 function main() {
   emailInput.addEventListener("blur", requiredValidator);
