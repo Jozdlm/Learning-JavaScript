9b9041b893af275fb22b092f683997e09b38b6a5
Jozuan Lopez <jozuanlopez@gmail.com>
2023-08-16 20:57:22 -0600

    Define las constantes y las funciones inciales del script

---
+++ form-validation/js/app.js
@@ -0,0 +1,11 @@
+const emailInput = document.getElementById("email");
+const asuntoInput = document.getElementById("asunto");
+const mensajeInput = document.getElementById("mensaje");
+
+function requiredValidator() {}
+
+function emailValidator() {}
+
+function main() {}
+
+main();
