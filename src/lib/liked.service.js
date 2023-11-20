1c5ef2f290da10e967c9f81e956424294f7d3dbd
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-19 21:25:25 -0600

    Guarda la lista de favoritos en el local-storage

--- src/lib/liked.service.js
+++ src/lib/liked.service.js
@@ -2,7 +2,13 @@ import { getMovieById } from "./movies.service.js";
 
 const likedMovies = [];
 
+const saveListChanges = () => {
+  localStorage.setItem('liked-movies', JSON.stringify(likedMovies));
+};
+
 export const addToLikedList = async (movieId) => {
+  // TODO: watch if the movie is already exist on the liked-list
   const movie = await getMovieById(movieId);
   likedMovies.push(movie);
+  saveListChanges();
 };
