6b632a0845d5f12f687be3e82b70aa9e61bf0935
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-19 22:22:23 -0600

    Agrega los valores al state de liked-movies

--- src/lib/liked.service.js
+++ src/lib/liked.service.js
@@ -1,19 +1,22 @@
 import { getMovieById } from "./movies.service.js";
 
-const likedMovies = [];
+let likedMovies = [];
 
 const saveListChanges = () => {
   localStorage.setItem("liked-movies", JSON.stringify(likedMovies));
 };
 
 export const getLikedMovies = () => {
-  return JSON.parse(localStorage.getItem("liked-movies") || "");
+  const loadedList = JSON.parse(localStorage.getItem("liked-movies") || "");
+  likedMovies = [...loadedList];
+  return likedMovies;
 };
 
 export const addToLikedList = async (movieId) => {
   // TODO: watch if the movie is already exist on the liked-list
   const movie = await getMovieById(movieId);
-  likedMovies.push(movie);
+  getLikedMovies();
+  likedMovies = [...likedMovies, movie];
   saveListChanges();
 };
 
